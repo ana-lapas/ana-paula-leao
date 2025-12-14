5d865c758f026fab63ffad583d0b16afc1e96555
ana Paula Lapas <anapaulalapas@gmail.com>
2025-12-14 15:35:21 -0300

    First page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to my portfolio.</h1>
+      <p>This is the main landing page of the application.</p>
+    </div>
+  );
+}
+
+export default Home;
